d207630a89e4e92a628f07b9b262ce2895cfecb7
dawidkurkiewicz <dawidkurkiewicz516@gmail.com>
2018-10-31 10:41:42 +0100

    pre task 1

--- script.js
+++ script.js
@@ -1,8 +1,46 @@
-const me = {
-    name: 'dawid',
-    JSskill: 1,
-    growJSskill: function () {
-        return this.JSskill = this.JSskill + 1
+// const growJSskill = function () {
+//     return this.JSskill = this.JSskill + 1
+// }
+
+
+// this.JSskill = this.JSskill + 1 = {
+//     name: 'dawid',
+//     JSskill: 1,
+//     growJSskill: growJSskill
+// }
+
+// const brother = {
+//     name: 'zygmunt',
+//     JSskill: 0,
+//     growJSskill: growJSskill
+// }
+
+// const brothers = {
+//     for (let i = 0; i > 100; i ++){
+
+//     }
+// }
+
+
+
+
+// me.growJSskill()
+// brother.growJSskill()
+
+
+const growJSskill = function (){
+    this.JSskill = this.JSskill + 1
+}
+const makePerson = function (name, initialSkill) {
+    return {
+        name: name,
+        JSskill: initialSkill,
+        growJSskill: growJSskill
     }
 }
-me.growJSskill(3)
+const me = makePerson('Dawid', 100)
+const brother = makePerson('jan', 10)
+
+
+me.growJSskill()
+brother.growJSskill()
