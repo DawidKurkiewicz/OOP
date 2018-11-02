b84517d3876827ed124fdcd5f140fd81063efcb1
dawidkurkiewicz <dawidkurkiewicz516@gmail.com>
2018-11-02 12:00:05 +0100

    done button and menu

--- ClickableButton.js
+++ ClickableButton.js
@@ -1,25 +1,18 @@
 function ClickableButton(label, handler) {
-    this.label = label
-    this.handler = handler
-    this.button = null
-    this.init()
+this.label = label
+this.handler = handler
+
+this.render ()
 }
 
-ClickableButton.prototype.init = function () {
-    this.render()
-}
+
 
 
 ClickableButton.prototype.render = function () {
-    this.button = document.createElement('button')
-    this.button.style.top = '100px'
-    this.button.style.right = '100px'
-    this.button.style.backgroundColor = 'yellow'
-    this.button.style.height = '100px'
-    this.button.style.width = '100px'
-    this.button.addEventListener('click', this.handler)
-
-    document.body.appendChild(this.button)
+    const button = document.createElement('button')
+    button.innerText = this.label
+    button.addEventListener('click', this.handler)
+    document.body.appendChild(button)
 
 }
 
--- SideBarMenu.js
+++ SideBarMenu.js
@@ -42,3 +42,4 @@ SideBarMenu.prototype.toggleMenu = function () {
     this.render()
 }
 
+
