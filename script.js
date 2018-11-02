744a6aa18a63b2dfec3b78db972ed9cf3250f2e8
dawidkurkiewicz <dawidkurkiewicz516@gmail.com>
2018-11-02 09:41:36 +0100

    sidebar menu

--- script.js
+++ script.js
@@ -1,22 +1,56 @@
-const myOwnNew = (constructorFn, argsArray) => {
-    const newInstance = Object.create(constructorFn.prototype)
+// function Dog(name) {
+//     this.sound = 'woof'
+//     this.name = name
+// }
 
-    constructorFn.apply(newInstance, argsArray)
-    return newInstance
+// Dog.prototype.makeSound = function () {
+//     console.log(this.sound)
+// }
+
+// Dog.prototype.sound = 'buuum'
+
+// const dog0 = Dog()
+// const dog1 = new Dog('klebuszek') 
+// const dog2 = new Dog('puszek')
+
+// dog1.makeSound()
+
+// const myOwnNew = (constructorFn, argsArray) => {
+//     const newInstance = Object.create(constructorFn.prototype)
+//     constructorFn.apply(newInstance, argsArray)
+//     return newInstance
+// }
+// function Dog(name) {
+//     this.sound = 'woof'
+//     this.name = name
+// }
+// Dog.prototype.makeSound = function () {
+//     console.log(this.sound)
+// }
+// Dog.prototype.sound = 'buuum'
+// const dog1 = myOwnNew (Dog, ['Puszek'])
+// dog1.makeSound()
+
+
+
+
+// function Person(name) {
+//     this.name = name;
+// }
+// Person.prototype.sayHello = function () {
+//     console.log(`Hi, I'm ${this.name}`)
+// }
+// const me = new Person('dawid');
+// me.sayHello();
+
+
+function SideBarMenu () {
+    this.isOpen = true
+    this.isOnTheLeft = true
+    this.isOneTheRight = false
+    this.bgColor = 'red'
+
 }
 
 
-
-function Dog(name) {
-    this.sound = 'woof'
-    this.name = name
-}
-
-Dog.prototype.makeSound = function () {
-    console.log(this.sound)
-}
-
-Dog.prototype.sound = 'buuum'
-
-const dog1 = myOwnNew (Dog, ['Puszek'])
-dog1.makeSound()
+const menu1 = new SideBarMenu();
