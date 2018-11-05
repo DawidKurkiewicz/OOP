5a7d13ac4853618a79e63522f35477d3cbf9f7a5
dawidkurkiewicz <dawidkurkiewicz516@gmail.com>
2018-11-05 15:06:51 +0100

    task4 calculator

--- part3/script.js
+++ part3/script.js
@@ -1,27 +1,45 @@
-class Animal {
-    constructor(name){
-        this.name=name
+class Calculator {
+    inputFirst(first) {
+        this.first = first
+    }
+    inputSecond(second) {
+        this.second = second
+    }
+    add() {
+        console.log(this.first + this.second)
+    }
+    subtract() {
+        console.log(this.first - this.second)
+    }
+    multiply() {
+        console.log(this.first * this.second)
+    }
+    divide() {
+        if (this.second === 0) {
+            console.log('nie dziel przez zero cholero')
+        } else {
+            console.log(this.first / this.second)
+        }
+    }
+    pow() {
+        let x = 1
+        for (let i = 1; i <= this.second; i++) {
+            x = this.first * x;
+        }
+        console.log(x)
     }
 }
-class Cat extends Animal{
-    constructor(name){
-        super(name)
-        this.sound = 'miau'
-    }
-    makeSound() {
-        console.log(this.sound)
-    }
-    changeSound(sound) {
-        this.sound = sound
-    }
-}
 
-const cat = new Cat('poooo')
 
-cat.makeSound()
 
-cat.changeSound('klaczek')
 
-cat.makeSound()
 
-console.log(cat)
+const calc = new Calculator()
+calc.inputFirst(5)
+calc.inputSecond(2)
+
+calc.add()
+calc.subtract()
+calc.multiply()
+calc.divide()
+calc.pow()
