2a7d663c01f9cb5a308d5f8039a99883ecda953f
dawidkurkiewicz <dawidkurkiewicz516@gmail.com>
2018-11-05 10:07:09 +0100

    added push function

--- part2/script.js
+++ part2/script.js
@@ -1,13 +1,12 @@
-function ToDo(){
+function ToDo() {
     this.tasks = []
 }
-
-ToDo.prototype.addTasks = function() {
-    new Task(text)
-
+ToDo.prototype.addTasks = function () {
+    this.tasks.push(new Task(text))
 }
-
-
-function Task (text) {
+function Task(text) {
     this.text = text
 }
+const toDo1 = new ToDo()
+toDo1.addTasks('wynies smieci')
+console.log(toDo1)
