3e9c392c97f89af448d714acc4fa206bd98cc1a1
dawidkurkiewicz <dawidkurkiewicz516@gmail.com>
2018-11-05 13:40:49 +0100

    task2

--- part3/script.js
+++ part3/script.js
@@ -1,29 +1,5 @@
-class Cat {
-    constructor(name) {
-        this.name = name
-    }
-    makeSound() {
-        console.log('Meoooooooow')
-    }
-}
-
-
-const cat1 = new Cat('Burek')
-console.log(cat1)
-
-
-cat1.makeSound()
-
-
-// function Cat(name) {
-//     this.name = name
-// }
-
-// const cat1 = new Cat('Burek')
-
-// Cat.prototype.makeSound = function() {
-//     console.log('Meoooooooow')
-// }
-
-// cat1.makeSound()
-//console.log(cat1)
+const makeCat = () => ({
+    makeSound: console.log('mia')
+    })
+const cat = makeCat()
+cat.makeSound()
