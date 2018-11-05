cfccf1de4e84d0859e11122864b5d3fe5fcb623f
dawidkurkiewicz <dawidkurkiewicz516@gmail.com>
2018-11-05 11:23:40 +0100

    added input and button

--- part2/script.js
+++ part2/script.js
@@ -1,12 +1,39 @@
 function ToDo() {
-    this.tasks = []
+    this.task = []
+    this.render()
 }
-ToDo.prototype.addTasks = function () {
-    this.tasks.push(new Task(text))
+ToDo.prototype.addTask = function (text) {
+    this.task.push(new Task(text))
+    this.render()
+}
+ToDo.prototype.render = function () {
+    document.body.innerHTML = ''
+    this.makeUI()
+    const list = document.createElement('ul')
+    this.task.forEach(task => {
+        const taskDiv = document.createElement('li')
+        taskDiv.innerText = task.text
+        list.appendChild(taskDiv)
+    })
+    document.body.appendChild(list)
+}
+ToDo.prototype.makeUI = function () {
+    const input = document.createElement('input')
+    const button = document.createElement('button')
+    button.innerText = 'dodaj task'
+        button.addEventListener(
+            'click',
+            () => this.addTask(input.value)
+            //(function() { this.addTask(input.value) }).bind(this)
+
+        )
+    document.body.appendChild(input)
+    document.body.appendChild(button)
+
 }
 function Task(text) {
     this.text = text
 }
 const toDo1 = new ToDo()
-toDo1.addTasks('wynies smieci')
-console.log(toDo1)
+
+
